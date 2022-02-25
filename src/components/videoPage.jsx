8dfb23c9544e86e3dadf0e1189bf50c50dff8b1e
JuBoks <89145094+JuBoks@users.noreply.github.com>
2022-02-25 17:30:07 +0900

    videoPage 기능 구현

--- src/components/videoPage.jsx
+++ src/components/videoPage.jsx
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams, useLocation } from 'react-router-dom';
+import Videos from './videos';
 
 const VideoPage = () => {
   const params = useParams();
   const location = useLocation();
-  console.log(location.state)
+  const video = location.state.video.snippet;
+  const [videos, setVideos] = useState();
+  console.log(video)
+  const requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  
+  // mount 됐을 때, 현재 영상의 제목으로 search 한 video list 우측 사이드바에 출력
+  useEffect(() => {
+    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&regionCode=KR&q=${video.title}&maxResults=25&key=${process.env.REACT_APP_API_KEY}`, requestOptions)
+    .then(response => response.text())
+    .then(result => {
+      result = JSON.parse(result);
+      const items = result && result.items ? result.items : [];
+      console.log(items);
+      setVideos(items);
+    })
+    .catch(error => console.log('error', error));
+  },[]);
+
   return (
     <div>
+      <div>
+        <div>
+          <iframe width="1280" height="720" 
+            src={`https://www.youtube.com/embed/${params.videoId}`}
+            title="YouTube video player" 
+            frameBorder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            allowFullScreen />
+        </div>
+        <div>
+          <div>{video.title}</div>
+          {video.description}
+        </div>
+      </div>
+      <div>
+        <Videos videos={videos} />
+      </div>
       <h1>{params.videoId}</h1>
     </div>
   );
